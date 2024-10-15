f975e4cff06e9c14d94da6b098ed2ce53bc4a0da
zsq <2811798415@qq.com>
2024-10-15 15:27:47 +0800

    ok

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,10 +3,9 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::sync::mpsc;
-use std::sync::Arc;
+
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -26,32 +25,39 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+fn send_tx(q: Arc<Mutex<Queue>>, tx: mpsc::Sender<u32>) {
+    let qc1 = Arc::clone(&q);
+    let qc2 = Arc::clone(&q);
 
-    thread::spawn(move || {
-        for val in &qc1.first_half {
+    let tx1 = tx.clone();
+    let tx2 = tx.clone();
+
+    let handle1 = thread::spawn(move || {
+        let q = qc1.lock().unwrap();
+        for val in &q.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-    thread::spawn(move || {
-        for val in &qc2.second_half {
+    let handle2 = thread::spawn(move || {
+        let q = qc2.lock().unwrap();
+        for val in &q.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
 }
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Queue::new();
-    let queue_length = queue.length;
+    let queue = Arc::new(Mutex::new(Queue::new()));
+    let queue_length = queue.lock().unwrap().length;
 
     send_tx(queue, tx);
 
@@ -62,5 +68,5 @@ fn main() {
     }
 
     println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length)
+    assert_eq!(total_received, queue_length);
 }
